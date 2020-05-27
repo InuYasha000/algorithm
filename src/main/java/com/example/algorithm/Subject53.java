aa1c7c4d8cc6b807b06702ffff2d975d07d8dac3
chengjj@ihdou.com <2457567745@qq.com>
2020-05-27 22:43:08 +0800

    44:字符串相乘 53:最大子序和

---
+++ src/main/java/com/example/algorithm/Subject53.java
@@ -0,0 +1,23 @@
+package com.example.algorithm;
+
+/**
+ * @Auther: cheng
+ * @Date: 2020/5/27 22:42
+ * @Description:最大子序和
+ */
+public class Subject53 {
+    public int maxSubArray(int[] nums) {
+        int arr[] = new int[nums.length];
+        arr[0] = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (arr[i - 1] < 0) {
+                arr[i] = nums[i];
+            } else {
+                arr[i] = arr[i - 1] + nums[i];
+            }
+            max = Math.max(max, arr[i]);
+        }
+        return max;
+    }
+}
