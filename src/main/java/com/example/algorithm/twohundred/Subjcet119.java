2518b4a509c671b6982fa2f8a67e213c64a23b8a
chengjj@ihdou.com <2457567745@qq.com>
2020-07-24 00:06:54 +0800

    118；杨辉三角 119：杨辉三角II

---
+++ src/main/java/com/example/algorithm/twohundred/Subjcet119.java
@@ -0,0 +1,23 @@
+package com.example.algorithm.twohundred;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Auther: cheng
+ * @Date: 2020/7/23 22:43
+ * @Description:杨辉三角II
+ */
+public class Subjcet119 {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        for(int i=1;i<=rowIndex;i++){
+            for(int j=1;j<i;j++){
+                list.set(j,list.get(j-1)+list.get(j));
+            }
+            list.add(1);
+        }
+        return list;
+    }
+}
---
+++ src/main/java/com/example/algorithm/twohundred/Subject118.java
@@ -0,0 +1,48 @@
+package com.example.algorithm.twohundred;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Auther: cheng
+ * @Date: 2020/7/23 21:00
+ * @Description:杨辉三角
+ * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ *
+ * 图见https://leetcode-cn.com/problems/pascals-triangle/
+ *
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ *
+ * 示例:
+ *
+ * 输入: 5
+ * 输出:
+ * [
+ *      [1],
+ *     [1,1],
+ *    [1,2,1],
+ *   [1,3,3,1],
+ *  [1,4,6,4,1]
+ * ]
+ *
+ */
+public class Subject118 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(numRows==0){
+            return res;
+        }
+        res.add(Arrays.asList(1));
+        for(int i=1;i<numRows;i++){
+            List<Integer> temp = new ArrayList<>();
+            temp.add(1);
+            for(int j=1;j<i;j++){
+                temp.add(res.get(i-1).get(j-1)+res.get(i-1).get(j));
+            }
+            temp.add(1);
+            res.add(temp);
+        }
+        return res;
+    }
+}
